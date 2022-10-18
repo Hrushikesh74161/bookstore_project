4fabbfb36887ee4213cc927741c20ef52a572be7
hrushikesh <hrushi74161@gmail.com>
2022-10-18 19:06:39 +0530

    tests created for homepage

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('home.html')
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
