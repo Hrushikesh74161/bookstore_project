54cb72cb1e2fac51da7b37b2c64dc12eaec2b99c
hrushikesh <hrushi74161@gmail.com>
2022-10-18 18:44:01 +0530

    pages app created, templates folder created

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -15,6 +15,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 # password = bookstoreproject
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',  # new
+    'pages.apps.PagesConfig',  # new
 ]
 
 MIDDLEWARE = [
@@ -59,7 +61,7 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
