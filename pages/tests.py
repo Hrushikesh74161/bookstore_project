ab4f51a5ec447d11cc978a74d56ac37b1151cd00
hrushikesh <hrushi74161@gmail.com>
2022-10-18 19:19:55 +0530

    tests for homepage

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from .views import HomePageView
+
+
+class HomePageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_homepage_content(self):
+        self.assertContains(self.response, 'Homepage')
+        self.assertNotContains(self.response, 'not homepage')
+
+    def test_homepage_url_resolves_to_homepageview(self):
+        view = resolve(reverse('home'))
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
