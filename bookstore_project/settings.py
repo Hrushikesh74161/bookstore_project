12c0b2c13e72d10186b6d18971596c638690d1a1
hrushikesh <hrushi74161@gmail.com>
2022-10-18 18:27:48 +0530

    custom user forms, custom user admin page, superuser created

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+# superuser
+# username - admin
+# password = bookstoreproject
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
