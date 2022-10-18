87d86f4d841ba32618be01fdf40629c93f086610
hrushikesh <hrushi74161@gmail.com>
2022-10-18 21:08:06 +0530

    login, logout functionality, signup functionality and tests

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,9 @@
-from django.test import TestCase
+from django.urls import reverse, resolve
+from django.test import TestCase, SimpleTestCase
 from django.contrib.auth import get_user_model
+
+from .forms import CustomUserCreationForm
+from .views import SignUpView
 
 
 class CustomUserTests(TestCase):
@@ -30,3 +34,24 @@ class CustomUserTests(TestCase):
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+
+class SignUpTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'not signup')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+
+    def test_signup_resolve_to_signupview(self):
+        view = resolve('/accounts/signup')
+        self.assertEqual(view.func.__name__, SignUpView.as_view().__name__)
