f27bc6af82870a80ad939f9c85ea2065c8ff7458
hrushikesh <hrushi74161@gmail.com>
2022-10-19 13:26:36 +0530

    static folders, static config in settings.py, about page and tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import AboutPageView, HomePageView
 
 
 class HomePageTests(SimpleTestCase):
@@ -23,3 +23,23 @@ class HomePageTests(SimpleTestCase):
     def test_homepage_url_resolves_to_homepageview(self):
         view = resolve(reverse('home'))
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+class AboutPageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_aboutpage_content(self):
+        self.assertContains(self.response, 'About Page')
+        self.assertNotContains(self.response, 'hot about page')
+
+    def test_aboutpage_url_resolves_to_aboutpageview(self):
+        view = resolve(reverse('about'))
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
