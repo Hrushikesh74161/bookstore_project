9bcb5917bc228dfea083f6d834533583487bd645
hrushikesh <hrushi74161@gmail.com>
2022-10-22 12:07:03 +0530

    reviews model created, tests for book_detail page for reviews

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,21 @@
+import site
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Review
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['review', 'book', 'author',]
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_diplay = ("title", "author", "price",)
+    inlines = [
+        ReviewInline
+    ]
--- books/tests.py
+++ books/tests.py
@@ -1,17 +1,28 @@
 from django.test import TestCase, Client
 from django.urls import reverse, resolve
+from django.contrib.auth import get_user_model
 
-from .models import Book
+from .models import Book, Review
 from .views import BookListView, BookDetailView
 
 
-class BookTests(TestCase):
+class BookModelTests(TestCase):
 
     def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='reviewuser',
+            email='reviewuser@email.com',
+            password='reviewuser1234',
+        )
         self.book = Book.objects.create(
             title='Harry Potter',
             author='JK Rowling',
             price='25.00',
+        )
+        self.review = Review.objects.create(
+            book = self.book,
+            review='excellent review',
+            author=self.user
         )
 
     def test_book_listing(self):
@@ -33,6 +44,7 @@ class BookTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'JK Rowling')
         self.assertTemplateUsed(response, 'books/book_detail.html')
+        self.assertContains(response, 'excellent review')
 
     def test_book_list_url_resolves_to_view(self):
         view = resolve(reverse('book_list'))
